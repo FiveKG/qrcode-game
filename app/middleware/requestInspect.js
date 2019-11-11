5540274d8d78b5a132cdc9a16ec149cddaccdc2f
liji <li16001600@163.com>
2019-11-11 17:50:31 +0800

    初始化

--- app/middleware/requestInspect.js
+++ app/middleware/requestInspect.js
@@ -13,25 +13,25 @@ module.exports = options => {
         let token = ctx.header.token;
         if (!token) {
             ctx.status = 403;
-            ctx.body = await ctx.helper.renderError(403, 'token invalid', '');
+            ctx.body = await ctx.helper.renderError(403, 'token invalid');
             return;
         }
         try {
             let user = await ctx.helper.decodeToken(token);
             ctx.logger.debug('user: ', user);
             if (!user['is_enable']) {
-                ctx.body = await ctx.helper.renderError(500001, '用户状态不可用', '');
+                ctx.body = await ctx.helper.renderError(500001, '用户状态不可用');
                 return;
             }
             if ('admin' != user['user_type']) {
-                ctx.body = await ctx.helper.renderError(500002, '权限不足', '');
+                ctx.body = await ctx.helper.renderError(500002, '权限不足');
                 return;
             }
             await next();
             return;
         } catch (e) {
             ctx.status = 403;
-            ctx.body = await ctx.helper.renderError(403, 'token invalid', '');
+            ctx.body = await ctx.helper.renderError(403, 'token invalid');
         }
     }
 }
