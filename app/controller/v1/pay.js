01491c6236706f89a1159215b3eace2d2b4bc746
吴恭鑫 <414901826@qq.com>
2019-11-29 21:24:22 +0800

    finish pay 2019-11-29 pm

---
+++ app/controller/v1/pay.js
@@ -0,0 +1,78 @@
+//@ts-check
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class PayController extends Controller {
+    async balance_logManage(){
+        const { ctx } = this;
+        await ctx.render('/pay/balance_logManage.html');
+    }
+    async pay_orderManage(){
+        const { ctx } = this;
+        await ctx.render('/pay/pay_orderManage.html');
+    }
+    async user_rechargeManage(){
+        const { ctx } = this;
+        await ctx.render('/pay/user_rechargeManage.html');
+    }
+
+
+
+    async getBalance_log() {
+        const { ctx, service, logger } = this;
+        try {
+            const reqData = ctx.request.body;
+            logger.debug('请求参数：%j', reqData);
+            let start = Number(reqData['limit']) * (Number(reqData['page']) -1);
+            let size = Number(reqData['limit']);
+            const result = await service.pay.findBalance_log(start, size, reqData['search'] || {});
+            if (result === null) {
+                ctx.body = await ctx.helper.renderError(500000, '系统出错');
+            } else {
+                ctx.body = await ctx.helper.renderSuccess(0, '', result);
+            }
+        } catch (e) {
+            logger.error(e);
+            ctx.body = await ctx.helper.renderError(500000, '系统出错');
+        }
+    }
+    async getPay_order() {
+        const { ctx, service, logger } = this;
+        try {
+            const reqData = ctx.request.body;
+            logger.debug('请求参数：%j', reqData);
+            let start = Number(reqData['limit']) * (Number(reqData['page']) -1);
+            let size = Number(reqData['limit']);
+            const result = await service.pay.findPay_order(start, size, reqData['search'] || {});
+            if (result === null) {
+                ctx.body = await ctx.helper.renderError(500000, '系统出错');
+            } else {
+                ctx.body = await ctx.helper.renderSuccess(0, '', result);
+            }
+        } catch (e) {
+            logger.error(e);
+            ctx.body = await ctx.helper.renderError(500000, '系统出错');
+        }
+    }
+    async getUser_recharge() {
+        const { ctx, service, logger } = this;
+        try {
+            const reqData = ctx.request.body;
+            logger.debug('请求参数：%j', reqData);
+            let start = Number(reqData['limit']) * (Number(reqData['page']) -1);
+            let size = Number(reqData['limit']);
+            const result = await service.pay.findUser_recharge(start, size, reqData['search'] || {});
+            if (result === null) {
+                ctx.body = await ctx.helper.renderError(500000, '系统出错');
+            } else {
+                ctx.body = await ctx.helper.renderSuccess(0, '', result);
+            }
+        } catch (e) {
+            logger.error(e);
+            ctx.body = await ctx.helper.renderError(500000, '系统出错');
+        }
+    }
+}
+
+module.exports = PayController;
