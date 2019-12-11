069192215ac04f91261d880606d02717447b17b8
吴恭鑫 <414901826@qq.com>
2019-12-11 11:34:24 +0800

    no pwd

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Description: run admin-site service
-<<<<<<< HEAD
 export DB_HOST=DB_HOST
 export DB_PORT=12432
 export DB_USER=DB_USER
@@ -13,18 +12,4 @@ export JWT_SECRET=JWT_SECRET
 export HASH_SECRET=HASH_SECRET
 export PROXY_HOST=PROXY_HOST
 export REDIS_PWD=REDIS_PWD
-=======
-export DB_HOST=DB_HOST
-export DB_PORT=12432
-export DB_USER=DB_USER
-export DB_PWD=DB_PWD
-export DB_NAME=DB_USER_db
-export REDIS_HOST=DB_HOST
-export REDIS_PORT=REDIS_PORT
-export REDIS_DB=6
-export JWT_SECRET=JWT_SECRET
-export HASH_SECRET=HASH_SECRET
-export PROXY_HOST=PROXY_HOST
-export REDIS_PWD=REDIS_PWD
->>>>>>> 2a74492d390c81417969618d9887ec42efa9fdc7
 npm start 
